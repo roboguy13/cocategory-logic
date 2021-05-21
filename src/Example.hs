321602b2f3885cf8ef344a2f432d9a4f03054c46
David Young <roboguy06@gmail.com>
2021-05-21 00:25:08 -0500

    Some renaming

--- src/Example.hs
+++ src/Example.hs
@@ -9,66 +9,16 @@ module Example where
 import           Implies
 import           Cocategory
 
-test :: Proof (p |- (p `And` p))
-test x = verify (and_elimL x)
+example1 :: Proof (p |- (p `And` p))
+example1 x = verify (and_elimL x)
 
-test2 :: forall p. Proof (p |- ((p `And` p) `And` p))
-test2 x = applyLPair (decompose x) go
+example2 :: forall p. Proof (p |- ((p `And` p) `And` p))
+example2 x = applyLPair (decompose x) go
   where
     go :: (p |- (p `And` p)) %1 -> ((p `And` p) |- ((p `And` p) `And` p)) %1 -> ()
     go y z =
-      test y <.> lemma z
+      example1 y <.> lemma z
 
     lemma :: Proof ((p `And` p) |- ((p `And` p) `And` p))
     lemma x = verify (and_elimL x)
-
-{-
-test2 :: forall a b (c :: Prop). (a |- T) %1-> b
-test2 x = applyLPair (decompose x) go2
-  where
-    -- go :: LPair (a |- c) (c |- T) %1-> b
-    -- go (LPair y z) = go2 y z
-
-    go2 :: (a |- c) %1 -> (c |- 'T) %1 -> b
-    go2 = undefined
--}
-
-
-  -- case decompose x of
-  --   LPair y z ->
-  --     let () = verify y
-  --         () = verify z in
-  --       undefined
-
--- test2 :: forall p. Proof (p |- ((p `And` p) `And` p))
--- test2 x = decomposeCont x (\y z ->
---   let () = verify y
---       () = verify z
---   in undefined
---   ) --go (decompose x)
-  -- case decompose x of
-  --   _ -> verify x
-  -- where
-  --   go :: Proof _
-  --   go (y, z) = case verify z of () -> verify y
-
-  -- case decompose x of
-  --   (y, z) ->
-  --     case verify (weaken y) of
-  --       () ->
-  --         test z
-
-  -- let (y, z) = decompose x
-  --     -- () = _
-  -- in _ --verify z
-
--- test' :: Proof ((p `And` p) |- p)
--- test' x = verify (and_elimL x)
-
--- test' :: Proof (T |- (T `And` T))
--- test' x =
---   let (y, z) = decompose x :: (T |- T, T |- (T `And` T))
---       (p, q) = decompose z :: (T |- T, T |- (T `And` T))
---   in
---   verify (and_intro y p)
 
