2ca8f2f989d8f3da8882fb89e67614a8699d6c11
David Young <roboguy06@gmail.com>
2021-05-21 00:29:05 -0500

    More linear types in DSL interface

--- src/Implies.hs
+++ src/Implies.hs
@@ -76,42 +76,42 @@ and_intro Implies Implies = Implies
 and_elimL :: forall x p q. (x |- (p `And` q)) %1-> (x |- p)
 and_elimL Implies = Implies
 
-and_elimR :: forall x p q. (x |- (p `And` q)) -> (x |- q)
+and_elimR :: forall x p q. (x |- (p `And` q)) %1-> (x |- q)
 and_elimR Implies = Implies
 
-or_introL :: forall x p q. (x |- p) -> (x |- (p `Or` q))
+or_introL :: forall x p q. (x |- p) %1-> (x |- (p `Or` q))
 or_introL Implies = Implies
 
-or_introR :: forall x p q. (x |- q) -> (x |- (p `Or` q))
+or_introR :: forall x p q. (x |- q) %1-> (x |- (p `Or` q))
 or_introR Implies = Implies
 
-or_elim :: forall x x' p q. (x |- (p `Or` q)) -> (p |- x') -> (q |- x') -> (x |- x')
+or_elim :: forall x x' p q. (x |- (p `Or` q)) %1-> (p |- x') %1-> (q |- x') %1-> (x |- x')
 or_elim Implies Implies Implies = Implies
 
-entails_intro :: forall x p q. ((x `And` p) |- q) -> (x |- (p `Entails` q))
+entails_intro :: forall x p q. ((x `And` p) |- q) %1-> (x |- (p `Entails` q))
 entails_intro Implies = Implies
 
-entails_elim :: forall x p q. (x |- (p `Entails` q)) -> (x |- p) -> (x |- q)
+entails_elim :: forall x p q. (x |- (p `Entails` q)) %1-> (x |- p) %1-> (x |- q)
 entails_elim Implies Implies = Implies
 
-not_intro :: forall x p q. (x |- (p `Entails` F)) -> (x |- Not p)
+not_intro :: forall x p q. (x |- (p `Entails` F)) %1-> (x |- Not p)
 not_intro Implies = Implies
 
-not_elim :: forall x p q. (x |- Not p) -> (x |- p) -> (x |- q)
+not_elim :: forall x p q. (x |- Not p) %1-> (x |- p) %1-> (x |- q)
 not_elim Implies Implies = Implies
 
 
 -- TODO: Are these structural rules necessary to be spelled out, or can
 -- they be derived from the previous rules within this particular logic?
-exchange :: forall x y p. ((x `And` y) |- p) -> ((y `And` x) |- p)
+exchange :: forall x y p. ((x `And` y) |- p) %1-> ((y `And` x) |- p)
 exchange Implies = Implies
 
 weaken :: forall x y p. (x |- p) %1-> ((x `And` y) |- p)
 weaken Implies = Implies
 
-contract :: forall x y p. ((x `And` y `And` y) |- p) -> (x `And` y |- p)
+contract :: forall x y p. ((x `And` y `And` y) |- p) %1-> (x `And` y |- p)
 contract Implies = Implies
 
-reassoc :: forall x y z p. ((x `And` y) `And` z |- p) -> (x `And` (y `And` z) |- p)
+reassoc :: forall x y z p. ((x `And` y) `And` z |- p) %1-> (x `And` (y `And` z) |- p)
 reassoc Implies = Implies
 
