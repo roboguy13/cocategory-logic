1c7fda3505a31df0ba1cdbfb841bd730d3cbbcc7
David Young <roboguy06@gmail.com>
2021-05-24 13:37:37 -0500

    Use (&) in example2

--- src/Example.hs
+++ src/Example.hs
@@ -4,25 +4,24 @@
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Example where
 
 import           Implies
 import           Cocategory
 
+import           Prelude.Linear ((&))
+
 example1 :: Proof (p |- (p && p))
 example1 x = verify (and_elimL x)
 
 -- To test 'decompose':
 example2 :: forall p. Proof (p |- ((p && p) && p))
-example2 x = applyPair (decompose x) go
+example2 x =
+  decompose x & \case
+    (y, z) -> example1 y <.> lemma z
   where
-    go ::    (p |- (p && p)) %1
-          -> ((p && p) |- ((p && p) && p)) %1
-          -> ()
-    go y z =
-      example1 y <.> lemma z
-
     lemma :: Proof ((p && p) |- ((p && p) && p))
     lemma x = verify (and_elimL x)
 
